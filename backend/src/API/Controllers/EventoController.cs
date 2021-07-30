33645f4937362c3f04c8211107b7967766099142
David <david_sjr@hotmail.com>
2021-07-30 15:39:56 -0300

    Adicionando o EF

--- backend/src/API/Controllers/EventoController.cs
+++ backend/src/API/Controllers/EventoController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using API.Data;
 using API.Models;
 using Microsoft.AspNetCore.Mvc;
 
@@ -10,40 +11,21 @@ namespace API.Controllers
     [Route("[controller]")]
     public class EventoController : ControllerBase
     {
-        private IEnumerable<Evento> _evento = new Evento[]
-            {
-                new Evento()
-                {
-                    EventoId = 1, 
-                    Tema = "Angular 11 e .NET 5",
-                    Local = "Poá",
-                    Lote = "1º Lote",
-                    QtdPessoas = 250,
-                    DataEvento = DateTime.Now.AddDays(2).ToString()
-                },
-                new Evento()
-                {
-                    EventoId = 2, 
-                    Tema = "Angular 11 e .NET 5",
-                    Local = "Remoto",
-                    Lote = "2º Lote",
-                    QtdPessoas = 450,
-                    DataEvento = DateTime.Now.AddDays(3).ToString()
-                },
-            };
-        public EventoController()
+        private readonly DataContext _context;
+        public EventoController(DataContext context)
         {
+            _context = context;
         }
 
         [HttpGet]
         public IEnumerable<Evento> Get()
         {
-            return _evento;
+            return _context.Eventos;
         }
         [HttpGet("{id}")]
-        public IEnumerable<Evento> Get(int id)
+        public Evento Get(int id)
         {
-            return _evento.Where(e => e.EventoId == id);
+            return _context.Eventos.FirstOrDefault(e => e.EventoId == id);
         }
     }
 }
